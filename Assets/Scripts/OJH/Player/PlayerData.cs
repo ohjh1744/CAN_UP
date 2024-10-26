3cf6a2dc4b8e0b7ecd2485f12f565b190346cdbe
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-10-27 01:01:36 +0900

    [FEAT] 점프력, 이동속도, 아이템소유여부, 아이템던지기세기 변수 추가

--- Assets/Scripts/OJH/Player/PlayerData.cs
+++ Assets/Scripts/OJH/Player/PlayerData.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
+// PlayerData는 추후에 프로퍼티 수정 예정.
 public class PlayerData : MonoBehaviour
 {
-    [SerializeField] private int _jumpNum;
+    [SerializeField] private int _jumpNum;    //mvp패턴 변수
 
     public int JumpNum { get {return _jumpNum; } set { _jumpNum = value; } }
     
-    [SerializeField] private int _fallNum;
+    [SerializeField] private int _fallNum;  //mvp패턴 변수
 
     public int FallNum { get { return _fallNum; } set { _fallNum = value; } }
+
+    [SerializeField] private float _jumpPower;
+
+    public float JumpPower {  get { return _jumpPower; } set { _jumpPower = value; } }
+
+    [SerializeField] private float _moveSpeed;
+
+    public float MoveSpeed {  get { return _moveSpeed; }  set { _moveSpeed = value; } }
+
+    [SerializeField] private bool _hasItem;
+
+    public bool HasItem { get { return _hasItem; } set { _hasItem = value; } }
+
+    [SerializeField] private float _throwPower;
+
+    public float ThrowPower { get { return _throwPower; } set { _throwPower = value; } }
 
 
 }
