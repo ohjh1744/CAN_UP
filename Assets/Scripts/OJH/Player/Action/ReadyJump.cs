816551358c34f45577ce94d882cfcb769f85fa8e
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-10-27 21:38:03 +0900

    [FEAT] BTREE Test 구현 기획자분들 의견 최근 버전 수용
    [FEAT] BTREE Test 구현 기획자분들 의견 최근 버전 수용

--- Assets/Scripts/OJH/Player/Action/ReadyJump.cs
+++ Assets/Scripts/OJH/Player/Action/ReadyJump.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class ReadyJump : PlayerAction
 {
-   
+    [SerializeField] private float _maxTime;
+
+    [SerializeField] private float _minTime;
+
+    [SerializeField] private float _canJumpTime;
+    
     public override BTNodeState DoAction()
     {
+        if ((Input.GetKeyUp(KeyCode.Space) || _canJumpTime > _maxTime) && !(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
+        {
+            Debug.Log("점프!");
+            _canJumpTime = 0;
+            return BTNodeState.Success;
+        }
+
         if (Input.GetKey(KeyCode.Space) && !(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
         {
+            _canJumpTime += Time.deltaTime;
             Debug.Log("점프력 차징 중");
+
             return BTNodeState.Running;
+
         }
-        else
-        {
-            return BTNodeState.Failure;
-        }
+       
+
+        return BTNodeState.Failure;
     }
     
 }
