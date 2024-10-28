54f9d5caef88662ade8da8068ba2bffd4f3db721
TaeHeon Ryoo <taroni55@naver.com>
2024-10-28 20:21:24 +0900

    [Feat] Base 캐릭터 이동 기능 구현

--- Assets/Scripts/YTH/Player/BaseCharacter/BaseAction/ActBaseReadyJump.cs
+++ Assets/Scripts/YTH/Player/BaseCharacter/BaseAction/ActBaseReadyJump.cs
@@ -9,22 +9,19 @@ public class ActBaseReadyJump : PlayerAction
     [SerializeField] Rigidbody _rigidbody;
     public override BTNodeState DoAction()
     {
+        if (Input.GetKey(KeyCode.Space) && !(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
+        {
+            _baseData.JumpPower += Time.deltaTime;
+            Debug.Log("점프력 차징 중");
+            return BTNodeState.Running;
+        }
 
         if ((Input.GetKeyUp(KeyCode.Space) || _baseData.CanJumpTime > _baseData.MaxTime) && !(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
         {
             Debug.Log("점프!");
+            _rigidbody.AddForce(Vector3.up * _baseData.JumpPower, ForceMode.Impulse);
             _baseData.CanJumpTime = 0;
-            _rigidbody.AddForce(Vector3.up * _baseData.JumpPower, ForceMode.Impulse);
             return BTNodeState.Success;
-        }
-
-        if (Input.GetKey(KeyCode.Space) && !(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
-        {
-            _baseData.CanJumpTime += Time.deltaTime;
-            _baseData.JumpPower++;
-            Debug.Log("점프력 차징 중");
-            _rigidbody.AddForce(Vector3.up * _baseData.JumpPower, ForceMode.Impulse);
-            return BTNodeState.Running;
         }
 
         return BTNodeState.Failure;
