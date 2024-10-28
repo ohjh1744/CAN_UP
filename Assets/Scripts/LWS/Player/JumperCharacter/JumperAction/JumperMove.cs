b5d9dede90f7da62188230bf4cc3ed92ca7c33a4
Glaz8212 <lws7129@naver.com>
2024-10-28 16:59:01 +0900

    [FEAT] Jumper Move 감속 기능 추가 (+ 투명 메테리얼 및 구체 생성)
    추후 기능 테스트 한번 더 하고, 인스펙터창으로 빼는 작업 필요

--- Assets/Scripts/LWS/Player/JumperCharacter/JumperAction/JumperMove.cs
+++ Assets/Scripts/LWS/Player/JumperCharacter/JumperAction/JumperMove.cs
@@ -8,6 +8,9 @@ public class JumperMove : PlayerAction
 
     private Rigidbody _rigidbody;
 
+    // 감속 비율
+    private float _decelerationFactor = 0.9f;
+
     // JumperData, Rigidbody 가져오기
     private void Start()
     {
@@ -26,11 +29,27 @@ public class JumperMove : PlayerAction
         }
 
         // A,D 입력시 이동 진행
-        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) && !Input.GetKey(KeyCode.Space))
-        {
+        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
+        {            
             float horizontalInput = Input.GetAxis("Horizontal");
-            _rigidbody.velocity = new Vector3(horizontalInput * _jumperData.MoveSpeed, _rigidbody.velocity.y, _rigidbody.velocity.z);
-            Debug.Log("이동 중");
+
+            // 현재 이동 방향이 반대인지 확인
+            bool isOppositeDirection = (horizontalInput < 0 && _rigidbody.velocity.x > 0) || (horizontalInput > 0 && _rigidbody.velocity.x < 0);
+
+            Debug.Log($"반대 방향 여부: {isOppositeDirection}");
+
+            // 반대방향 키를 눌렀을 때 감속 적용
+            if (isOppositeDirection)
+            {
+                _rigidbody.velocity = new Vector3(_rigidbody.velocity.x * (1 - _decelerationFactor), _rigidbody.velocity.y, _rigidbody.velocity.z);
+                Debug.Log("감속 중");
+            }
+            else
+            {
+                _rigidbody.velocity = new Vector3(horizontalInput * _jumperData.MoveSpeed, _rigidbody.velocity.y, _rigidbody.velocity.z);
+                Debug.Log("공중에서 이동 중");
+            }
+
             return BTNodeState.Running;
         }
         // 미 입력 시 Failure 반환
