77708f3b113e41e8594e7a5fa4d700934c200c4c
TaeHeon Ryoo <taroni55@naver.com>
2024-10-28 20:56:42 +0900

    [Feat] 차징 점프 추가 기능 구현

--- Assets/Scripts/YTH/Player/BaseCharacter/BaseAction/ActBaseJump.cs
+++ Assets/Scripts/YTH/Player/BaseCharacter/BaseAction/ActBaseJump.cs
@@ -10,12 +10,22 @@ public class ActBaseJump : PlayerAction
 
     public override BTNodeState DoAction()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && !(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
+        if (Input.GetKey(KeyCode.Space) && !(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
         {
+            _data.JumpPower += Time.deltaTime * 6f;
+            Debug.Log("점프력 차징 중");
+            return BTNodeState.Running;
+        }
+        else if ((Input.GetKeyUp(KeyCode.Space)) && !(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
+        {
+            if (_data.JumpPower >= _data.MaxJumpPower)
+            {
+                _data.JumpPower = _data.MaxJumpPower;
+            }
             _rigidbody.AddForce(Vector3.up * _data.JumpPower, ForceMode.Impulse);
             Debug.Log("점프!");
-           
-            return BTNodeState.Running;
+            _data.JumpPower = 0;
+            return BTNodeState.Success;
         }
         else
         {
