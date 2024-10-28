d8a9dde85738eb2bba2f8d3137d91ac5c27828c8
Glaz8212 <lws7129@naver.com>
2024-10-28 14:53:10 +0900

    [FIX] 기획 의도 반영 ( 점프 쿨타임 추가 )

--- Assets/Scripts/LWS/Player/JumperCharacter/JumperAction/JumperJump.cs
+++ Assets/Scripts/LWS/Player/JumperCharacter/JumperAction/JumperJump.cs
@@ -7,9 +7,15 @@ public class JumperJump : PlayerAction
     private JumperData _jumperData;
 
     private Rigidbody _rigidbody;
-    
+
     // 땅에 닿아있는지 판단할 Grounded 불 변수
     private bool _isGrounded;
+
+    // 점프 간격 시간 0.2초
+    private float _jumpCooldown = 0.2f;
+
+    // 게임 시작하자마자 점프 할 수 있도록, -1을 초기값으로 지정
+    private float _groundedTime = -1f;
 
     // DoAction 재정의
     public override BTNodeState DoAction()
@@ -19,12 +25,17 @@ public class JumperJump : PlayerAction
 
         _rigidbody = GameObject.FindObjectOfType<Rigidbody>();
 
-        // 땅에 닿을 때마다 점프 실행
-        if (_isGrounded)
+        // 현재 시간 = 땅에 닿아있는 시간 + 쿨타임보다 긴 지 확인
+        if (_isGrounded && Time.time >= _groundedTime + _jumpCooldown)
         {
             _rigidbody.velocity = new Vector3(_rigidbody.velocity.x, _jumperData.JumpPower, 0);
+            _isGrounded = false; // 점프 직후에 땅에 닿지 않은 상태로 설정
+            return BTNodeState.Running;
         }
-        return BTNodeState.Running;
+        else
+        {
+            return BTNodeState.Failure;
+        }
     }
 
     // 땅에 닿아있는 프레임마다 _isGrounded = true -> 점프 실행
@@ -33,14 +44,7 @@ public class JumperJump : PlayerAction
         if (collision.gameObject.CompareTag("Ground"))
         {
             _isGrounded = true;
-        }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            _isGrounded = false;
+            _groundedTime = Time.time;
         }
     }
 }
