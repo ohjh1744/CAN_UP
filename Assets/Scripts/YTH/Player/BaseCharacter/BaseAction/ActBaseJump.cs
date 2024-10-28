90d933b296d626792c4b576b10b816d21a3e0a06
TaeHeon Ryoo <taroni55@naver.com>
2024-10-28 23:20:54 +0900

    [Fix] 레이캐스트로 IsGrounded 확인하지 못하는 버그 수정

--- Assets/Scripts/YTH/Player/BaseCharacter/BaseAction/ActBaseJump.cs
+++ Assets/Scripts/YTH/Player/BaseCharacter/BaseAction/ActBaseJump.cs
@@ -10,23 +10,31 @@ public class ActBaseJump : PlayerAction
 
     public override BTNodeState DoAction()
     {
-        if (Input.GetKey(KeyCode.Space) && !(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
-        {
-            _data.JumpPower += Time.deltaTime * 6f;
+        if (Input.GetKey(KeyCode.Space) && !(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))) // space 누르고 있는 상태
+        {                                                                                         
+            _data.JumpPower += Time.deltaTime * 6f;  // 점프력 상승중
             Debug.Log("점프력 차징 중");
-            return BTNodeState.Running;
-        }
-        else if ((Input.GetKeyUp(KeyCode.Space)) && !(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
-        {
-            if (_data.JumpPower >= _data.MaxJumpPower)
+
+            if (_data.JumpPower >= _data.MaxJumpPower)  // 점프력이 최대 점프력을 넘어서면 최대 점프력으로 점프                                                                                             
             {
                 _data.JumpPower = _data.MaxJumpPower;
+                _rigidbody.AddForce(Vector3.up * _data.JumpPower, ForceMode.Impulse);
+                _data.JumpPower = 0;
+                return BTNodeState.Success;
             }
-            _rigidbody.AddForce(Vector3.up * _data.JumpPower, ForceMode.Impulse);
+            return BTNodeState.Running;
+        }
+
+        if ((Input.GetKeyUp(KeyCode.Space)) &&  !(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))) // space 떼거나
+        {                                                                                              // 점프력이 최대 점프력에 도달 시
+            _rigidbody.AddForce(Vector3.up * _data.JumpPower, ForceMode.Impulse); // 점프
             Debug.Log("점프!");
+
+            _data.IsGrounded = false;
             _data.JumpPower = 0;
             return BTNodeState.Success;
         }
+
         else
         {
             return BTNodeState.Failure;
