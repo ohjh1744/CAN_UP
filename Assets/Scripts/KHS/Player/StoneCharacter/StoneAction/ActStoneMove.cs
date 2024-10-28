31a110d19a8bcd59cd0299baeec6737fa0ce6455
gojjang10 <gojjang2@gmail.com>
2024-10-28 17:04:08 +0900

    [Feat] 알까기 캐릭터 기능 구현 완료

--- Assets/Scripts/KHS/Player/StoneCharacter/StoneAction/ActStoneMove.cs
+++ Assets/Scripts/KHS/Player/StoneCharacter/StoneAction/ActStoneMove.cs
@@ -4,8 +4,57 @@ using UnityEngine;
 
 public class ActStoneMove : PlayerAction
 {
+    [SerializeField] StoneData _data;
+
     public override BTNodeState DoAction()
     {
-        throw new System.NotImplementedException();
+        Debug.Log("날아가는 로직 진입함");
+        ApplyForceToPlayer();
+        return BTNodeState.Success;
+    }
+
+    private void ApplyForceToPlayer()
+    {
+        // 선택된 플레이어가 없으면 return
+        if (_data.SelectedPlayer == null) return;
+
+        Vector3 releasePosition = _data.MainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, _data.MainCamera.WorldToScreenPoint(_data.SelectedPlayer.transform.position).z));
+        releasePosition.z = _data.SelectedPlayer.transform.position.z;
+
+        // 방향 벡터
+        float dragDistance = Vector3.Distance(_data.DragStartPoint, releasePosition);
+
+        if (dragDistance > _data.MaxPullDistance)
+        {
+            dragDistance = _data.MaxPullDistance;
+            releasePosition = _data.DragStartPoint + (releasePosition - _data.DragStartPoint).normalized;
+        }
+        if (dragDistance >= 1f && dragDistance <= _data.MaxPullDistance * (1/3))
+        {
+            _data.JumpPower *= 1 / 3; 
+        }
+        else if (dragDistance > _data.MaxPullDistance * (1 / 3) && dragDistance <= _data.MaxPullDistance * (2 / 3))
+        {
+            _data.JumpPower *= 2 / 3;
+        }
+        else if (dragDistance > _data.MaxPullDistance * (2 / 3) && dragDistance <= _data.MaxPullDistance)
+        {
+            _data.JumpPower *= 1;
+        }
+
+        Vector3 forceDirection = (_data.DragStartPoint - releasePosition).normalized;
+
+        // 캐릭터의 위치와 마우스커서 위치의 거리를 float값으로 저장
+        float forceMagnitude = dragDistance * _data.JumpPower;
+
+        Rigidbody rb = _data.SelectedPlayer.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            // 힘을 가해주는 위치 설정
+            rb.AddForceAtPosition(forceDirection * forceMagnitude, _data.SelectedPlayer.transform.position, ForceMode.Impulse);
+            //rb.AddForce(forceDirection * forceMagnitude, ForceMode.Impulse);
+        }
+
+        _data.SelectedPlayer = null;
     }
 }
