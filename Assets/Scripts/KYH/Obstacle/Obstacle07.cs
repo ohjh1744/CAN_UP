b6acbebd336a4de95ee8d9a5b9c3cc0532c11c7d
KimYonai <msb0714@gmail.com>
2024-10-29 15:41:03 +0900

    [Feat] Obstacle01 기능 구현

--- Assets/Scripts/KYH/Obstacle/Obstacle07.cs
+++ Assets/Scripts/KYH/Obstacle/Obstacle07.cs
@@ -8,7 +8,7 @@ public class Obstacle07 : MonoBehaviour
     [SerializeField] Transform _stopPos;
     [SerializeField] Rigidbody _rigid;
 
-    public void TouchPlayer(PlayerController player)
+    public void MovePlatform(PlayerController player)
     {
         // 멈추는 위치에 빈 오브젝트 설치할 것
         transform.position = Vector3.Lerp(gameObject.transform.position, _stopPos.position, 2.0f);
--- Assets/Scripts/KYH/Obstacle/Obstacle09.cs
+++ Assets/Scripts/KYH/Obstacle/Obstacle09.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class Obstacle09 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject _player;
+    [SerializeField] float _moveSpeed;
 
-    // Update is called once per frame
-    void Update()
+    public void MoveWithPlayer(PlayerController player)
     {
         
     }
