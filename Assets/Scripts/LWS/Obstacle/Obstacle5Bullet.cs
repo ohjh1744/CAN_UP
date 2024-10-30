3b3f2852888658ca0c161401429e2989219380bf
Glaz8212 <lws7129@naver.com>
2024-10-31 00:10:11 +0900

    [FEAT] 5번 방해물 기능 수정 (대포)

---
+++ Assets/Scripts/LWS/Obstacle/Obstacle5Bullet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle5Bullet : MonoBehaviour
+{
+    // 부딪혔을 때 플레이어를 밀 힘
+    [SerializeField] private float pushForce;
+
+    private void Start()
+    {
+        Destroy(gameObject, 5f);
+    }
+
+    public void PushPlayer(PlayerController player)
+    {
+        // 플레이어의 rigid를 가져옴
+        Rigidbody rigid = player.GetComponent<Rigidbody>();
+
+        Vector3 pushDir = (rigid.transform.position - transform.position).normalized;
+        rigid.AddForce(pushDir * pushForce, ForceMode.Impulse);
+    }
+}
