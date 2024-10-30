6931a8bcb992fa1b7b742104019bcb5e4b3b59e5
TaeHeon Ryoo <taroni55@naver.com>
2024-10-30 12:29:05 +0900

    [Fix] 캐릭터 점프 과정에서 점프력 적용되지 않는 버그 수정

--- Assets/Scripts/YTH/Player/BaseCharacter/BaseAction/ActBaseWalk.cs
+++ Assets/Scripts/YTH/Player/BaseCharacter/BaseAction/ActBaseWalk.cs
@@ -43,6 +43,5 @@ public class ActBaseWalk : PlayerAction
         {
             return BTNodeState.Failure;
         }
-
     }
 }
--- Assets/Scripts/YTH/Player/BaseCharacter/BaseCondition/CondBaseCanMove.cs
+++ Assets/Scripts/YTH/Player/BaseCharacter/BaseCondition/CondBaseCanMove.cs
@@ -6,23 +6,27 @@ public class CondBaseCanMove : PlayerCondition
 
     [SerializeField] Rigidbody _rigidbody;
 
+    [SerializeField] float _dropRayPosY = 0.1f;
     public override bool DoCheck()
     {
-        Debug.Log("작동 중");
-        Vector3 pos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        Vector3 pos = new Vector3(transform.position.x, transform.position.y + _dropRayPosY, transform.position.z);
         Ray ray = new Ray(pos, Vector3.down);
 
-        //Debug.Log("레이케스트 시작");
-        if (Physics.Raycast(ray, out RaycastHit hit, 0.2f))
+        Debug.Log("레이케스트 시작");
+        if (Physics.Raycast(ray, out RaycastHit hit, 0.3f))
         {
             Debug.DrawRay(pos, Vector3.down * 0.3f, Color.red);
             if (hit.collider.CompareTag("Ground"))
             {
                 _data.IsGrounded = true;
-                Debug.Log("바닥감지");
                 return true;
             }
         }
+        else
+        {
+            _data.IsGrounded = false;
+        }
+
 
         if (_data.IsStiff == true || _data.IsGrounded == false) //물리적으로 밀쳐진 상태
         {
