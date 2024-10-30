31390e386b2ccda8ed6e38f1f7634b1cfecd0eea
TaeHeon Ryoo <taroni55@naver.com>
2024-10-30 12:27:51 +0900

    [Fix] 점프력 적용 안 되는 버그 수정

--- Assets/Scripts/YTH/Player/BaseCharacter/BaseAction/ActBaseJump.cs
+++ Assets/Scripts/YTH/Player/BaseCharacter/BaseAction/ActBaseJump.cs
@@ -8,61 +8,85 @@ public class ActBaseJump : PlayerAction
 
     [SerializeField] Animator _animator;
 
+    [SerializeField] float _startJumpPositionY = 0.3f;
     public override BTNodeState DoAction()
     {
         Vector3 _jumpDirectionR = new Vector3(1, 2, 0).normalized;
         Vector3 _jumpDirectionL = new Vector3(-1, 2, 0).normalized;
 
-        if (Input.GetKey(KeyCode.Space) && !(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))) // space 누르고 있는 상태
+        //점프 차징 맥스, 최대점프력으로 점프
+        if (_data.JumpPower >= _data.MaxJumpPower && (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.Space)))
         {
-            _data.JumpPower += Time.deltaTime * 6f;  // 점프력 상승중
-            Debug.Log("점프력 차징 중");
+            _data.JumpPower = _data.MaxJumpPower;
 
-            //점프력 최대 도달 시 수직 점프
-            if (_data.JumpPower >= _data.MaxJumpPower)
+            if (Input.GetKey(KeyCode.A))
             {
-                _data.JumpPower = _data.MaxJumpPower;
-                _rigidbody.AddForce(Vector3.up * _data.JumpPower, ForceMode.Impulse);
-                JumpEvent();
-                return BTNodeState.Success;
+                _rigidbody.AddForce(_jumpDirectionL * _data.JumpPower, ForceMode.Impulse);
             }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                _rigidbody.AddForce(_jumpDirectionR * _data.JumpPower, ForceMode.Impulse);
+            }
+            else
+            {
+                _rigidbody.AddForce(Vector3.up * _data.JumpPower, ForceMode.Impulse);
+            }
+            JumpEvent();
+            Debug.Log("최대 점프");
+            return BTNodeState.Success;
+        }
 
+        if (Input.GetKey(KeyCode.Space))// space 누르고 있는 상태
+        {
+
+            _data.JumpPower += Time.deltaTime * 6f;
+            //회전 적용
+            if (Input.GetKey(KeyCode.A))
+            {
+                _rigidbody.velocity = Vector3.zero;
+                transform.forward = Vector3.left;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                _rigidbody.velocity = Vector3.zero;
+                transform.forward = Vector3.right;
+            }
+            else
+            {
+                _rigidbody.velocity = Vector3.zero;
+                transform.forward = Vector3.back;
+            }
+            Debug.Log("점프력 차징 중");
             return BTNodeState.Running;
         }
 
         //좌로 점프
-        else if ((Input.GetKeyUp(KeyCode.Space)) && (Input.GetKey(KeyCode.A)))
+        if ((Input.GetKeyUp(KeyCode.Space)) && ((Input.GetKey(KeyCode.A) || (Input.GetKeyUp(KeyCode.A)))))
         {
-            //점프력 최대 도달 시 (최대 점프력으로) 좌로 점프
-            if (_data.JumpPower >= _data.MaxJumpPower && (Input.GetKey(KeyCode.A)))                      
-            {                                                                                            //조건문 수정해서 점프력 차징이 A/D 키 눌렀을때 계속 이어지게 수정
-                _data.JumpPower = _data.MaxJumpPower;                                                    //점프력 적용 문제
-                _rigidbody.AddForce(_jumpDirectionL * _data.JumpPower, ForceMode.Impulse);
-                JumpEvent();
-                return BTNodeState.Success;
-            }
-
             _rigidbody.AddForce(_jumpDirectionL * _data.JumpPower, ForceMode.Impulse);
             JumpEvent();
+            Debug.Log("좌로 살짝 점프");
             return BTNodeState.Success;
         }
 
         // 우로 점프
-        else if ((Input.GetKeyUp(KeyCode.Space)) && (Input.GetKey(KeyCode.D)))
+        if ((Input.GetKeyUp(KeyCode.Space)) && ((Input.GetKey(KeyCode.D) || (Input.GetKeyUp(KeyCode.D)))))
         {
-            //점프력 최대 도달 시 (최대 점프력으로) 우로 점프
-            if (_data.JumpPower >= _data.MaxJumpPower && (Input.GetKey(KeyCode.D)))
-            {
-                _data.JumpPower = _data.MaxJumpPower;
-                _rigidbody.AddForce(_jumpDirectionR * _data.JumpPower, ForceMode.Impulse);
-                JumpEvent();
-                return BTNodeState.Success;
-            }
-
             _rigidbody.AddForce(_jumpDirectionR * _data.JumpPower, ForceMode.Impulse);
             JumpEvent();
+            Debug.Log("우로 살짝 점프");
             return BTNodeState.Success;
         }
+
+        // 수직 점프
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            _rigidbody.AddForce(Vector3.up * _data.JumpPower, ForceMode.Impulse);
+            JumpEvent();
+            Debug.Log("수직 살짝 점프");
+            return BTNodeState.Success;
+        }
+
 
 
         else
@@ -74,6 +98,7 @@ public class ActBaseJump : PlayerAction
     //점프 시 공통적으로 발생하는 함수
     public void JumpEvent()
     {
+        transform.position = new Vector3(transform.position.x, transform.position.y + 0.3f, transform.position.z);
         _data.IsGrounded = false;
         _animator.SetTrigger("Jump");
         _data.JumpPower = 0;
