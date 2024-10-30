ef75c31d6b2dfcce22b511bddd7f5f2b34fde98a
KimYonai <msb0714@gmail.com>
2024-10-30 23:39:27 +0900

    [Feat] Obstacle01 기능을 다음 점프 강화로 수정

--- Assets/Scripts/KYH/Obstacle/Obstacle01.cs
+++ Assets/Scripts/KYH/Obstacle/Obstacle01.cs
@@ -5,10 +5,47 @@ using UnityEngine;
 
 public class Obstacle01 : MonoBehaviour
 {
-    [SerializeField] private PlayerData _data;
-    
+    [SerializeField] BaseData _baseData;
+
+    [SerializeField] PlayerData _playerData;
+
+    [SerializeField] private bool _isIncrease;
+
     public void ForcedJump(PlayerController player)
     {
-        _data.JumpPower *= 2;
+        if (_isIncrease == true)
+            return;
+
+        if (player.gameObject.tag == "Base")
+        {
+            _baseData = player.GetComponent<BaseData>();
+            _baseData.MaxJumpPower *= 2;
+            _isIncrease = true;
+        }
+        else
+        {
+            _playerData = player.GetComponent<PlayerData>();
+            _playerData.JumpPower *= 2;
+            _isIncrease = true;
+        }
+    }
+
+    public void CancelJump(PlayerController player)
+    {
+        if (_isIncrease == false)
+            return;
+
+        if (player.gameObject.tag == "Base")
+        {
+            _baseData = player.GetComponent<BaseData>();
+            _baseData.MaxJumpPower /= 2;
+            _isIncrease = false;
+        }
+        else
+        {
+            _playerData = player.GetComponent<PlayerData>();
+            _playerData.JumpPower /= 2;
+            _isIncrease = false;
+        }
     }
 }
