6c3c4ac06718945fb892052a3f0c29d31598dffc
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-10-31 23:54:17 +0900

    [Create]CSVParser 및 IObjectPosition
    csv형식으로 플랫폼 및 기믹 포지션을 내보내기 위한
    기본 스크립트 생성

---
+++ Assets/Scripts/OJH/CsvParser/CsvParser.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Unity.PlasticSCM.Editor.WebApi;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CsvParser : MonoBehaviour
+{
+
+    //[SerializeField] private IObjectPosition[] _objectPositions;
+
+
+    //private StringBuilder _sb = new StringBuilder();
+
+    //private void Awake()
+    //{
+    //    Vector3toString();
+
+         
+    //}
+
+    //private void Vector3toString()
+    //{
+    //    for(int i = 0; i < _objectPositions.Length; i++)
+    //    {
+    //        char x = (char)_objectPositions[i].Position.x;
+    //        char y = (char)_objectPositions[i].Position.y;
+    //        char z = (char)_objectPositions[i].Position.z;
+    //    }
+
+    //    StringBuilder _tempSb = new StringBuilder();
+    //    _tempSb.Append();
+    //    List<string>[] _stringDatas = new List<string>[_objectPositions.Length];
+
+    //    _stringDatas.AddRange();
+    //}
+
+    //private void MakeCSVFile(string.join)
+    //{
+    //    _sb.AppendLine();
+    //}
+}
