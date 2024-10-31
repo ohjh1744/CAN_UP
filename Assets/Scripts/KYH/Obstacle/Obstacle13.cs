4653b0f147372266dd0dee551cec0e00129fb5b0
KimYonai <msb0714@gmail.com>
2024-10-31 16:10:08 +0900

    [Feat] Obstacle13 아이템 상호작용 기능 추가

--- Assets/Scripts/KYH/Obstacle/Obstacle13.cs
+++ Assets/Scripts/KYH/Obstacle/Obstacle13.cs
@@ -4,35 +4,56 @@ using UnityEngine;
 
 public class Obstacle13 : MonoBehaviour, IResetObject
 {
+    // Reset 시 시작 각도 저장용 변수
     private Quaternion _startRot;
 
+    // Reset 시 사용할 스테이지 목차
     [SerializeField] private int _stageNum;
     public int StageNum { get; set; }
 
-    [SerializeField] private float _rotateSpeed;
-
+    // 발판 회전 각도
     [SerializeField] private float _rotateAngle;
 
+    // 발판이 회전했는지 체크
     [SerializeField] private bool _isRotate;
 
+    // 회전축 오브젝트
     [SerializeField] private GameObject _rotatePos;
 
+    // 발판의 Rigidbody 참조용
     [SerializeField] private Rigidbody _rigid;
 
     private Coroutine _rotateRoutine;
 
     private void Start()
     {
+        // 처음의 회전 각도를 저장
         _startRot = transform.rotation;
     }
 
+    // 발판 회전 함수(플레이어용)
     public void RotatePlatform(PlayerController player)
     {
+        // 이미 회전했으면 예외 처리
         if (_isRotate == true)
             return;
 
+        // _rotatePos를 회전축으로 _rotateAngle의 정도만큼 회전
         gameObject.transform.RotateAround(_rotatePos.transform.position, Vector3.forward, _rotateAngle);
-        _isRotate = true;
+        _isRotate = true;   // 회전했다고 체크
+
+    }
+
+    // 발판 회전 함수(아이템용)
+    public void RotatePlatform(Item item)
+    {
+        // 이미 회전했으면 예외 처리
+        if (_isRotate == true)
+            return;
+
+        // _rotatePos를 회전축으로 _rotateAngle의 정도만큼 회전
+        gameObject.transform.RotateAround(_rotatePos.transform.position, Vector3.forward, _rotateAngle);
+        _isRotate = true;   // 회전했다고 체크
 
     }
 
