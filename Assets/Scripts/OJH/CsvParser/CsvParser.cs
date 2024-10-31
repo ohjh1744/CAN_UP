b0144d818696b7dd15bf1f9b7387006535328392
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-11-01 03:22:22 +0900

    [FEAT] CSV Parser 구현 완료.
    save 버튼 누를때마다 저장하도록 구현

--- Assets/Scripts/OJH/CsvParser/CsvParser.cs
+++ Assets/Scripts/OJH/CsvParser/CsvParser.cs
@@ -11,41 +11,54 @@ public class CsvParser : MonoBehaviour
     [SerializeField] private IObjectPosition[] _objectPositions;
 
     //임시용
-    private Vector3[] _positions = { new Vector3(1, 1, 0), new Vector3(1, 1, 2) };
-    private string[] _names = { "OBstacle1", "Obstacle2" };
+    //private Vector3[] _positions = { new Vector3(1, 1, 0), new Vector3(1, 1, 2) };
+    //private string[] _names = { "OBstacle1", "Obstacle2" };
 
     private StringBuilder _sb = new StringBuilder();
 
+    private string _savePath;
+
     private void Awake()
     {
-        Vector3toString();
+        // 저장경로 user/appdata/LocalLow/Can_Up/Save
+        _savePath = Application.persistentDataPath + "/Save";
+        CreateCsv();
     }
-    
-    private void Vector3toString()
+
+    [ContextMenu("Save")]
+    private void CreateCsv()
     {
-        for(int i = 0; i < _positions.Length; i++)
+        
+        //csv형식으로 stringbuilder에 저장.
+        for(int i = 0; i < _objectPositions.Length; i++)
         {
             StringBuilder _tempSb = new StringBuilder();
 
-            _tempSb.Append(_names[i]);
+            _tempSb.Append(_objectPositions[i].Name);
             _tempSb.Append(",");
-            _tempSb.Append(_positions[i].x);
+            _tempSb.Append(_objectPositions[i].Position.x);
             _tempSb.Append(",");
-            _tempSb.Append(_positions[i].y);
+            _tempSb.Append(_objectPositions[i].Position.y);
             _tempSb.Append(",");
-            _tempSb.Append(_positions[i].z);
+            _tempSb.Append(_objectPositions[i].Position.z);
+            _tempSb.Append("\n");
 
-            //Debug.Log(_tempSb.ToString());
-
-            _sb.AppendLine(string.Join(",", _tempSb));
-
-            Debug.Log(_sb.ToString());
+            _sb.Append(_tempSb);
         }
 
-       
-        
+        // 저장경로가 존재하지않다면 생성. 있다면 기존꺼 사용.
+        if(Directory.Exists(_savePath) == false)
+        {
+            Directory.CreateDirectory(_savePath);
+        }
 
- 
+        // WRite 작성.
+        File.WriteAllText(_savePath + "/Save.csv", string.Join("\n", _sb));
+
+
+
+
+
     }
 
 
