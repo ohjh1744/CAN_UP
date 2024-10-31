be182cf0fb4f9f0104e8eb7da9799d3435773d69
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-11-01 02:05:41 +0900

    [FEAT} CSV Parser csv형식 구현은 상태 성공

--- Assets/Scripts/OJH/CsvParser/CsvParser.cs
+++ Assets/Scripts/OJH/CsvParser/CsvParser.cs
@@ -1,43 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
-using Unity.PlasticSCM.Editor.WebApi;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CsvParser : MonoBehaviour
 {
 
-    //[SerializeField] private IObjectPosition[] _objectPositions;
+    [SerializeField] private IObjectPosition[] _objectPositions;
+
+    //юс╫ц©К
+    private Vector3[] _positions = { new Vector3(1, 1, 0), new Vector3(1, 1, 2) };
+    private string[] _names = { "OBstacle1", "Obstacle2" };
+
+    private StringBuilder _sb = new StringBuilder();
+
+    private void Awake()
+    {
+        Vector3toString();
+    }
+    
+    private void Vector3toString()
+    {
+        for(int i = 0; i < _positions.Length; i++)
+        {
+            StringBuilder _tempSb = new StringBuilder();
+
+            _tempSb.Append(_names[i]);
+            _tempSb.Append(",");
+            _tempSb.Append(_positions[i].x);
+            _tempSb.Append(",");
+            _tempSb.Append(_positions[i].y);
+            _tempSb.Append(",");
+            _tempSb.Append(_positions[i].z);
+
+            //Debug.Log(_tempSb.ToString());
+
+            _sb.AppendLine(string.Join(",", _tempSb));
+
+            Debug.Log(_sb.ToString());
+        }
+
+       
+        
+
+ 
+    }
 
 
-    //private StringBuilder _sb = new StringBuilder();
-
-    //private void Awake()
-    //{
-    //    Vector3toString();
-
-         
-    //}
-
-    //private void Vector3toString()
-    //{
-    //    for(int i = 0; i < _objectPositions.Length; i++)
-    //    {
-    //        char x = (char)_objectPositions[i].Position.x;
-    //        char y = (char)_objectPositions[i].Position.y;
-    //        char z = (char)_objectPositions[i].Position.z;
-    //    }
-
-    //    StringBuilder _tempSb = new StringBuilder();
-    //    _tempSb.Append();
-    //    List<string>[] _stringDatas = new List<string>[_objectPositions.Length];
-
-    //    _stringDatas.AddRange();
-    //}
-
-    //private void MakeCSVFile(string.join)
-    //{
-    //    _sb.AppendLine();
-    //}
 }
