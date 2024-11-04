02c1bbd11170340e1c528914a97c8bb578a29c1a
TaeHeon Ryoo <taroni55@naver.com>
2024-11-04 09:45:07 +0900

    [Fix] 레이캐스트 오버랩스피어로 변경
    레이캐스트가 플랫폼위에서 안 닿는 문제로 인해 오버랩스피어 방식으로 변경

--- Assets/Scripts/YTH/Player/BaseCharacter/BaseCondition/CondBaseCanMove.cs
+++ Assets/Scripts/YTH/Player/BaseCharacter/BaseCondition/CondBaseCanMove.cs
@@ -1,29 +1,50 @@
+using System.Drawing;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CondBaseCanMove : PlayerCondition
 {
     [SerializeField] BaseData _data;
 
-    [SerializeField] float _dropRayPosY = 0.1f;
+    [SerializeField] float _dropRayPosY;
+
+    [SerializeField] Vector3 _size;
+
+    private RaycastHit hit;
     public override bool DoCheck()
     {
-
         Vector3 pos = new Vector3(transform.position.x, transform.position.y + _dropRayPosY, transform.position.z);
-        Ray ray = new Ray(pos, Vector3.down);
 
-        if (Physics.Raycast(ray, out RaycastHit hit, 0.3f))
+        Collider[] hit = Physics.OverlapBox(pos, _size, Quaternion.identity);
+
+        for (int i = 0; i < hit.Length; ++i)
         {
-            Debug.DrawRay(pos, Vector3.down * 0.3f, Color.red);
-            if (hit.collider.CompareTag("Ground") || hit.collider.CompareTag("ObstacleTri") || hit.collider.CompareTag("ObstacleCol"))
+            if (hit[i].CompareTag("Ground") || hit[i].CompareTag("ObstacleTri") || hit[i].CompareTag("ObstacleCol"))
             {
                 _data.IsGrounded = true;
                 return true;
             }
+            else
+            {
+                _data.IsGrounded = false;
+            }
         }
-        else
-        {
-            _data.IsGrounded = false;
-        }
+
+
+
+       // //Ray ray = new Ray(pos, Vector3.down);
+       //
+       // if (Physics.BoxCast(pos, transform.lossyScale / 4.0f, Vector3.down, out  hit, transform.rotation, 0.3f))
+       // {
+       //     
+       //     Debug.DrawRay(pos, Vector3.down * 0.3f, Color.red);
+       //     if (hit.collider.CompareTag("Ground") || hit.collider.CompareTag("ObstacleTri") || hit.collider.CompareTag("ObstacleCol"))
+       //     {
+       //         _data.IsGrounded = true;
+       //         return true;
+       //     }
+       // }
+       
 
 
         if (_data.IsStiff == true || _data.IsGrounded == false) //물리적으로 밀쳐진 상태
@@ -36,5 +57,12 @@ public class CondBaseCanMove : PlayerCondition
             Debug.Log("점프 가능 ! ");
             return true;
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 gizmos = new Vector3(transform.position.x, transform.position.y + _dropRayPosY, transform.position.z);
+
+        Gizmos.DrawWireCube( gizmos,  _size);
     }
 }
