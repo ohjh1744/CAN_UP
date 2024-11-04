f1efb2743b92206703609ddf6d330493b56be408
KimYonai <msb0714@gmail.com>
2024-11-04 17:33:01 +0900

    [Fix] 카메라 전환 1차 수정
    최적화가 조금 더 필요할 것이라고 판단되어 추가 수정 진행할 예정입니다.

--- Assets/Scripts/OJH/BTree/BTSelector.cs
+++ Assets/Scripts/OJH/BTree/BTSelector.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static Unity.VisualScripting.Metadata;
 
 
 //첫 노드부터 확인, 성공하거나 Running 중인 노드가 있다면 바로 리턴. 하나도 없다면 Failure 리턴.
